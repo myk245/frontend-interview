9dda73c0bb7fc159dd7cb94253d8b57f19dd6c2d
myk245 <myk245@nyu.edu>
2021-09-16 16:39:43 -0400

    add basic styling for form validation error message

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import "./App.css";
 const App = () => (
   <main className="App">
     <h1 className="App-title">Risk Assessment</h1>
-    <div>You must select at least one value for each question</div>
+    <div className="validation-error">You must select at least one value for each question</div>
     <form>
       {questions.map((question) => (
         <Question key={question.id} question={question} />
