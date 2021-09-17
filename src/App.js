36c198e43cd2862c6c14bc3cb5e3236007bad733
myk245 <myk245@nyu.edu>
2021-09-16 23:49:00 -0400

    implement simple version of form submission

--- src/App.js
+++ src/App.js
@@ -1,20 +1,31 @@
+import { useState } from 'react';
 import Question from "./Question";
 import questions from "./questions";
 import "./App.css";
 
-const App = () => (
-  <main className="App">
-    <h1 className="App-title">Risk Assessment</h1>
-    <div className="validation-error">You must select at least one value for each question</div>
-    <form>
-      {questions.map((question) => (
-        <Question key={question.id} question={question} />
-      ))}
-      <div>
-        <button type="submit">Next</button>
-      </div>
-    </form>
-  </main>
-);
+const App = () => {
+  const [selected, setSelected] = useState([]);
+  
+  const submitForm = (event) => {
+    event.preventDefault();
+    console.log(selected);
+    console.log(event.target);
+  }
+
+  return (
+      <main className="App">
+        <h1 className="App-title">Risk Assessment</h1>
+        <div className="validation-error">You must select at least one value for each question</div>
+        <form onSubmit={submitForm}>
+          {questions.map((question) => (
+            <Question key={question.id} question={question} />
+          ))}
+          <div>
+          <button type="submit">Next</button>
+          </div>
+        </form>
+      </main>
+    );
+}
 
 export default App;
